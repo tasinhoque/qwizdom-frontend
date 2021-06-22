6095f1dfb650e8432193e6085d29cec90c53c49e
Tasin Hoque <tasinhoque@gmail.com>
2021-06-22 14:35:59 +0600

    Integrate with backend

--- src/components/DashboardBody.js
+++ src/components/DashboardBody.js
@@ -1,39 +1,20 @@
 import React, { useState } from 'react';
-import { useEffect } from 'react';
 import { SingleCard } from '../components';
 import Pagination from '@material-ui/lab/Pagination';
-import { Typography, TextField, Grid } from '@material-ui/core';
-import api from '../api';
+import { Typography, TextField } from '@material-ui/core';
 import { withRouter } from 'react-router-dom';
 import CircularProgress from '@material-ui/core/CircularProgress';
 
 function DashboardBody({
   loading,
-  setLoading,
   totalPages,
-  setTotalPages,
   quizzes,
-  setQuizzes,
+  page,
+  setPage,
+  name,
+  setName,
 }) {
-  const [page, setPage] = useState(1);
-  const [name, setName] = useState('');
-
-  useEffect(async () => {
-    const signedIn = localStorage.getItem('refreshToken');
-    if (!signedIn) {
-      props.history.push('/');
-    }
-    setLoading(true);
-    api
-      .getQuizzes(`page=${page}&limit=4`)
-      .then(res => {
-        setTotalPages(res.data.totalPages);
-        setQuizzes(res.data.results);
-        console.log(res);
-        setLoading(false);
-      })
-      .catch(e => {});
-  }, [page]);
+  const [tempName, setTempName] = useState(name);
 
   const pageChange = (_event, num) => {
     setPage(num);
@@ -41,18 +22,7 @@ function DashboardBody({
 
   const keyPress = async e => {
     if (e.keyCode === 13) {
-      try {
-        setLoading(true);
-        const response = await api.getQuizzes(
-          `page=${page}&limit=4&name=${name}`
-        );
-
-        setQuizzes(response.data.results);
-        setTotalPages(response.data.totalPages);
-        setLoading(false);
-      } catch (error) {
-        console.log(error);
-      }
+      setName(tempName);
     }
   };
 
@@ -60,8 +30,6 @@ function DashboardBody({
     <>
       <div
         style={{
-          // display: 'flex',
-          // justifyContent: 'center',
           padding: '20px 0px 35px 0px',
           position: 'relative',
         }}
@@ -72,9 +40,9 @@ function DashboardBody({
         <TextField
           variant="outlined"
           style={{ position: 'absolute', right: '100px' }}
-          value={name}
+          value={tempName}
           onKeyDown={keyPress}
-          onChange={e => setName(e.target.value)}
+          onChange={e => setTempName(e.target.value)}
         />
       </div>
       {loading ? (
--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,6 +1,7 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { Grid } from '@material-ui/core';
 
+import api from '../api';
 import { Header, DashboardBody, DashboardSidebar } from '../components';
 
 export default function Dashboard() {
@@ -19,14 +20,19 @@ export default function Dashboard() {
   const [loading, setLoading] = useState(true);
   const [totalPages, setTotalPages] = useState(0);
   const [quizzes, setQuizzes] = useState([]);
+  const [page, setPage] = useState(1);
+  const [name, setName] = useState('');
+
+  const [queryString, setQueryString] = useState('');
 
   const dashboardBodyProps = {
     loading,
-    setLoading,
     totalPages,
-    setTotalPages,
     quizzes,
-    setQuizzes,
+    page,
+    setPage,
+    name,
+    setName,
   };
 
   const dashboardSidebarProps = {
@@ -48,22 +54,74 @@ export default function Dashboard() {
     setSurveySelected,
   };
 
-  // useEffect(async () => {
-  //   const signedIn = localStorage.getItem('refreshToken');
-  //   if (!signedIn) {
-  //     props.history.push('/');
-  //   }
-  //   setLoading(true);
-  //   api
-  //     .getQuizzes(`page=${page}&limit=4`)
-  //     .then(res => {
-  //       setTotalPages(res.data.totalPages);
-  //       setQuizzes(res.data.results);
-  //       console.log(res);
-  //       setLoading(false);
-  //     })
-  //     .catch(e => {});
-  // }, [page]);
+  const updateQueryString = () => {
+    let str = `page=${page}&limit=4`;
+
+    if (testSelected) {
+      str += '&isTest=true';
+    } else if (surveySelected) {
+      str += '&isTest=false';
+    }
+
+    if (timeBoundSelected) {
+      str += '&isTimeBound=true';
+    } else if (notTimeBoundSelected) {
+      str += '&isTimeBound=false';
+    }
+
+    if (scheduledSelected) {
+      str += '&isScheduled=true';
+    } else if (unscheduledSelected) {
+      str += '&isScheduled=false';
+    }
+
+    setQueryString(str);
+  };
+
+  useEffect(async () => {
+    const signedIn = localStorage.getItem('refreshToken');
+    if (!signedIn) {
+      props.history.push('/');
+    }
+
+    try {
+      setLoading(true);
+      updateQueryString();
+      const response = await api.getQuizzes(queryString);
+
+      setQuizzes(response.data.results);
+      setTotalPages(response.data.totalPages);
+      setLoading(false);
+    } catch (error) {
+      console.log(error);
+    }
+  }, []);
+
+  useEffect(async () => {
+    try {
+      setLoading(true);
+      const response = await api.getQuizzes(queryString);
+
+      setQuizzes(response.data.results);
+      setTotalPages(response.data.totalPages);
+      setLoading(false);
+    } catch (error) {
+      console.log(error);
+    }
+  }, [queryString]);
+
+  useEffect(() => {
+    updateQueryString();
+  }, [
+    surveySelected,
+    testSelected,
+    scheduledSelected,
+    unscheduledSelected,
+    timeBoundSelected,
+    notTimeBoundSelected,
+    page,
+    name,
+  ]);
 
   return (
     <>
