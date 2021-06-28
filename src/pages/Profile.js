95317f24df747569b92588845d125c11a03dac6c
Enan Ajmain <3nan.ajmain@gmail.com>
2021-06-28 20:46:50 +0600

    Profile: switch inputs on only when editing

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,10 +1,14 @@
 import { useState, useEffect } from 'react';
 import { fade, makeStyles } from '@material-ui/core/styles';
 import { Grid } from '@material-ui/core';
-import { TextField } from '@material-ui/core';
 import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
+import React from 'react';
+import Input from '@material-ui/core/Input';
+import InputLabel from '@material-ui/core/InputLabel';
+import TextField from '@material-ui/core/TextField';
+import FormControl from '@material-ui/core/FormControl';
 
 import api from '../api';
 import { Header, DashboardBody, DashboardSidebar } from '../components';
@@ -16,18 +20,29 @@ const useStyles = makeStyles(theme => ({
     // width: '100%',
   },
   editButton: {
-    marginTop: theme.spacing(2),
-    marginRight: theme.spacing(2),
+    marginTop: theme.spacing(4),
+    marginLeft: theme.spacing(1),
+    marginRight: theme.spacing(1),
   },
   avatar: {
     width: theme.spacing(30),
     height: theme.spacing(30),
     marginTop: theme.spacing(5),
+    marginBottom: theme.spacing(5),
+  },
+  textField: {
+    '& input': {
+      color: '#000000',
+    },
+    '& input:disabled': {
+      color: '#444444',
+    },
   },
 }));
 
 export default function Profile() {
   const classes = useStyles();
+  const [formEnabled, enableForm] = useState(!false);
 
   return (
     <>
@@ -42,37 +57,46 @@ export default function Profile() {
               className={classes.avatar}
             />
           </Grid>
-          <Grid item md={12} xs={12}>
-            <TextField
-              defaultValue="Remy Sharp"
-              className={classes.userName}
-              inputProps={{
-                min: 0,
-                style: { textAlign: 'center' },
-              }}
-            ></TextField>
-          </Grid>
-          <Grid item md={12} xs={12}>
-            <TextField
-              defaultValue="aaron@gmail.com"
-              className={classes.userName}
-              inputProps={{ min: 0, style: { textAlign: 'center' } }}
-            ></TextField>
-          </Grid>
-          <Grid item md={12} xs={12}>
-            <TextField
-              defaultValue="01202828392919"
-              className={classes.userName}
-              inputProps={{ min: 0, style: { textAlign: 'center' } }}
-            ></TextField>
-          </Grid>
+          <form noValidate autoComplete="off">
+            <Grid item md={12} xs={12}>
+              <TextField
+                defaultValue="Remy Sharp"
+                disabled={formEnabled}
+                label="Name"
+                className={classes.textField}
+                inputProps={{
+                  min: 0,
+                  style: { textAlign: 'center' },
+                }}
+              ></TextField>
+            </Grid>
+            <Grid item md={12} xs={12}>
+              <TextField
+                label="Email"
+                defaultValue="aaron@gmail.com"
+                disabled={formEnabled}
+                className={classes.textField}
+                inputProps={{ min: 0, style: { textAlign: 'center' } }}
+              ></TextField>
+            </Grid>
+            <Grid item md={12} xs={12}>
+              <TextField
+                label="Phone"
+                defaultValue="01202828392919"
+                disabled={formEnabled}
+                className={classes.textField}
+                inputProps={{ min: 0, style: { textAlign: 'center' } }}
+              ></TextField>
+            </Grid>
+          </form>
         </Grid>
-        <Grid container justify="flex-end">
+        <Grid container justify="center">
           <Button
             type="submit"
             variant="contained"
             color="primary"
             className={classes.editButton}
+            onClick={() => enableForm(false)}
           >
             Edit
           </Button>
@@ -81,6 +105,7 @@ export default function Profile() {
             variant="contained"
             color="primary"
             className={classes.editButton}
+            onClick={() => enableForm(true)}
           >
             Save
           </Button>
