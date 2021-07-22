c8754331131275d7a6db6e600ec7552a440b8b95
Enan Ajmain <3nan.ajmain@gmail.com>
2021-07-22 20:33:35 +0600

    Dash: move filter-chips to the top

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,13 +1,34 @@
 import { useRef, useState, useEffect } from 'react';
-import { Grid } from '@material-ui/core';
-import { fade, makeStyles } from '@material-ui/core/styles';
-import { Typography, TextField, InputAdornment } from '@material-ui/core';
+import {
+  Chip,
+  Avatar,
+  Grid,
+  Typography,
+  TextField,
+  InputAdornment,
+  Select,
+  Input,
+  FormControl,
+  MenuItem,
+} from '@material-ui/core';
+
+import { useTheme, makeStyles } from '@material-ui/core/styles';
 import Pagination from '@material-ui/lab/Pagination';
-import SearchIcon from '@material-ui/icons/Search';
 import CircularProgress from '@material-ui/core/CircularProgress';
+import SearchIcon from '@material-ui/icons/Search';
+import { Done as DoneIcon } from '@material-ui/icons';
 
 import api from '../api';
 import { Header, SingleCard } from '../components';
+
+function getStyles(name, personName, theme) {
+  return {
+    fontWeight:
+      personName.indexOf(name) === -1
+        ? theme.typography.fontWeightRegular
+        : theme.typography.fontWeightMedium,
+  };
+}
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -19,9 +40,88 @@ const useStyles = makeStyles(theme => ({
   },
   pageHeader: {
     width: '100%',
+    margin: theme.spacing(0, 9, 1, 9),
+  },
+  filterChipsContainer: {
     margin: theme.spacing(0, 9, 5, 9),
   },
 }));
+
+const ITEM_HEIGHT = 48;
+const ITEM_PADDING_TOP = 8;
+const MenuProps = {
+  PaperProps: {
+    style: {
+      maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
+      width: 250,
+    },
+  },
+};
+
+const ClickableChip = ({
+  selected,
+  setSelected,
+  label,
+  classes,
+  secondChipSelected,
+  setSecondChipSelected,
+}) => (
+  <Chip
+    className={classes.chip}
+    avatar={
+      selected ? (
+        <Avatar>
+          <DoneIcon />
+        </Avatar>
+      ) : (
+        <></>
+      )
+    }
+    label={label}
+    clickable
+    color="primary"
+    onClick={() => {
+      if (selected) {
+        setSelected(false);
+      } else {
+        if (secondChipSelected) {
+          setSecondChipSelected(false);
+        }
+        setSelected(true);
+      }
+    }}
+  />
+);
+
+const ChipGroup = ({
+  classes,
+  headerText,
+  firstChipText,
+  secondChipText,
+  firstChipSelected,
+  setFirstChipSelected,
+  secondChipSelected,
+  setSecondChipSelected,
+}) => (
+  <Grid item className={classes.chipGroup}>
+    <ClickableChip
+      classes={classes}
+      selected={firstChipSelected}
+      setSelected={setFirstChipSelected}
+      label={firstChipText}
+      secondChipSelected={secondChipSelected}
+      setSecondChipSelected={setSecondChipSelected}
+    />
+    <ClickableChip
+      classes={classes}
+      selected={secondChipSelected}
+      setSelected={setSecondChipSelected}
+      label={secondChipText}
+      secondChipSelected={firstChipSelected}
+      setSecondChipSelected={setFirstChipSelected}
+    />
+  </Grid>
+);
 
 export default function Dashboard() {
   const classes = useStyles();
@@ -45,6 +145,24 @@ export default function Dashboard() {
   const [queryString, setQueryString] = useState('');
 
   const [tempName, setTempName] = useState(name);
+
+  const [names, setNames] = useState([]);
+  const [personName, setPersonName] = useState([]);
+  const theme = useTheme();
+
+  const handleChange = event => {
+    const arr = event.target.value;
+    setPersonName(arr);
+
+    const ids = [];
+    names.forEach(({ name, id }) => {
+      if (arr.includes(name)) {
+        ids.push(id);
+      }
+    });
+
+    setCategoryIds(ids);
+  };
 
   const pageChange = (_event, num) => {
     setPage(num);
@@ -128,6 +246,10 @@ export default function Dashboard() {
 
       setQuizzes(response.data.results);
       setTotalPages(response.data.totalPages);
+
+      response = await api.getCategories();
+      setNames(response.data);
+
       setLoading(false);
     } catch (error) {
       console.log(error);
@@ -186,6 +308,86 @@ export default function Dashboard() {
                 ),
               }}
             />
+          </Grid>
+        </Grid>
+        <Grid container spacing={1} className={classes.filterChipsContainer}>
+          <Grid
+            container
+            justify="flex-start"
+            item
+            md={8}
+            className={classes.filterChips}
+          >
+            <ChipGroup
+              classes={classes}
+              // headerText="Quiz Type"
+              firstChipText="Test"
+              secondChipText="Survey"
+              firstChipSelected={testSelected}
+              secondChipSelected={surveySelected}
+              setFirstChipSelected={setTestSelected}
+              setSecondChipSelected={setSurveySelected}
+            />
+            <ChipGroup
+              classes={classes}
+              // headerText="Schedule Type"
+              firstChipText="Scheduled"
+              secondChipText="Unscheduled"
+              firstChipSelected={scheduledSelected}
+              secondChipSelected={unscheduledSelected}
+              setFirstChipSelected={setScheduledSelected}
+              setSecondChipSelected={setUnscheduledSelected}
+            />
+            <ChipGroup
+              classes={classes}
+              // headerText="Time"
+              firstChipText="Time Bound"
+              secondChipText="Not Time Bound"
+              firstChipSelected={timeBoundSelected}
+              secondChipSelected={notTimeBoundSelected}
+              setFirstChipSelected={setTimeBoundSelected}
+              setSecondChipSelected={setNotTimeBoundSelected}
+            />
+          </Grid>
+          <Grid
+            container
+            justify="flex-end"
+            item
+            md={4}
+            className={classes.filterChips}
+          >
+            <FormControl className={classes.formControl} variant="filled">
+              <Select
+                labelId="demo-mutiple-chip-filled-label"
+                id="demo-mutiple-chip"
+                multiple
+                value={personName}
+                onChange={handleChange}
+                input={<Input id="select-multiple-chip" />}
+                renderValue={selected => (
+                  <div className={classes.chips}>
+                    {selected.map(value => (
+                      <Chip
+                        key={value}
+                        label={value}
+                        className={classes.muiChip}
+                      />
+                    ))}
+                  </div>
+                )}
+                MenuProps={MenuProps}
+              >
+                {names.map(({ name, id }) => (
+                  <MenuItem
+                    key={id}
+                    value={name}
+                    style={getStyles(name, personName, theme)}
+                  >
+                    {name}
+                  </MenuItem>
+                ))}
+              </Select>
+            </FormControl>
           </Grid>
         </Grid>
         {loading ? (
