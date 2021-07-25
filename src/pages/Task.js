d20c4cfe9977a0b5c8ec2c9123d714b955fc7ec7
Enan Ajmain <3nan.ajmain@gmail.com>
2021-07-25 23:12:48 +0600

    Add tabs in tasks page

--- src/pages/Task.js
+++ src/pages/Task.js
@@ -12,10 +12,51 @@ import {
 import { makeStyles } from '@material-ui/core/styles';
 import React, { useEffect, useState } from 'react';
 import { useHistory } from 'react-router';
+import PropTypes from 'prop-types';
+import AppBar from '@material-ui/core/AppBar';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
+import Divider from '@material-ui/core/Divider';
+
 import api from '../api';
 import { Header } from '../components';
 
+function TabPanel(props) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+    >
+      {value === index && <Box p={3}>{children}</Box>}
+    </div>
+  );
+}
+
+TabPanel.propTypes = {
+  children: PropTypes.node,
+  index: PropTypes.any.isRequired,
+  value: PropTypes.any.isRequired,
+};
+
+function a11yProps(index) {
+  return {
+    id: `simple-tab-${index}`,
+    'aria-controls': `simple-tabpanel-${index}`,
+  };
+}
+
 const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1,
+    backgroundColor: theme.palette.background.paper,
+  },
   headerText: {
     marginTop: '40px',
   },
@@ -26,6 +67,12 @@ const Task = () => {
   const classes = useStyles();
   const [pendingCounts, setPendingCounts] = useState([]);
   const [upcomingQuizzes, setUpcomingQuizzes] = useState([]);
+
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
 
   useEffect(async () => {
     try {
@@ -41,78 +88,94 @@ const Task = () => {
     <>
       <Header />
       <Container maxWidth="md">
-        <h1 className={classes.headerText}>Pending Submissions</h1>
-        <Paper>
-          <TableContainer>
-            <Table stickyHeader>
-              <TableHead>
-                <TableRow>
-                  <TableCell align="center">Quiz Name</TableCell>
-                  <TableCell align="center">Pending Submission Count</TableCell>
-                  <TableCell align="center">Submissions Page</TableCell>
-                </TableRow>
-              </TableHead>
-              <TableBody>
-                {pendingCounts.map(({ quiz, count }) => (
-                  <TableRow>
-                    <TableCell align="center">{quiz.name}</TableCell>
-                    <TableCell align="center">{count}</TableCell>
-                    <TableCell align="center">
-                      <Button
-                        onClick={() =>
-                          history.push(`/quiz/${quiz.id}/submissions`)
-                        }
-                        variant="contained"
-                        color="primary"
-                      >
-                        visit
-                      </Button>
-                    </TableCell>
-                  </TableRow>
-                ))}
-              </TableBody>
-            </Table>
-          </TableContainer>
+        <Paper square style={{ marginTop: '10px' }}>
+          <Tabs
+            centered
+            value={value}
+            onChange={handleChange}
+            aria-label="simple tabs example"
+            variant="fullWidth"
+          >
+            <Tab label="Pending Submissions" {...a11yProps(0)} />
+            <Tab label="Upcoming Quizzes" {...a11yProps(1)} />
+          </Tabs>
         </Paper>
-        <h1 className={classes.headerText}>Upcoming Quizzes</h1>
-        <Paper>
-          <TableContainer>
-            <Table stickyHeader>
-              <TableHead>
-                <TableRow>
-                  <TableCell align="center">Quiz Name</TableCell>
-                  <TableCell align="center">Start Time</TableCell>
-                  <TableCell align="center">Quiz Home Page</TableCell>
-                </TableRow>
-              </TableHead>
-              <TableBody>
-                {upcomingQuizzes.map(quiz => (
+        <TabPanel value={value} index={0}>
+          <Paper>
+            <TableContainer>
+              <Table stickyHeader>
+                <TableHead>
                   <TableRow>
-                    <TableCell align="center">{quiz.name}</TableCell>
+                    <TableCell align="center">Quiz Name</TableCell>
                     <TableCell align="center">
-                      {new Date(quiz.startTime).toLocaleDateString('en-US', {
-                        minute: '2-digit',
-                        hour: 'numeric',
-                        day: 'numeric',
-                        month: 'long',
-                        year: 'numeric',
-                      })}
+                      Pending Submission Count
                     </TableCell>
-                    <TableCell align="center">
-                      <Button
-                        onClick={() => history.push(`/quiz/${quiz.id}/home`)}
-                        variant="contained"
-                        color="primary"
-                      >
-                        visit
-                      </Button>
-                    </TableCell>
+                    <TableCell align="center">Submissions Page</TableCell>
                   </TableRow>
-                ))}
-              </TableBody>
-            </Table>
-          </TableContainer>
-        </Paper>
+                </TableHead>
+                <TableBody>
+                  {pendingCounts.map(({ quiz, count }) => (
+                    <TableRow>
+                      <TableCell align="center">{quiz.name}</TableCell>
+                      <TableCell align="center">{count}</TableCell>
+                      <TableCell align="center">
+                        <Button
+                          onClick={() =>
+                            history.push(`/quiz/${quiz.id}/submissions`)
+                          }
+                          variant="contained"
+                          color="primary"
+                        >
+                          visit
+                        </Button>
+                      </TableCell>
+                    </TableRow>
+                  ))}
+                </TableBody>
+              </Table>
+            </TableContainer>
+          </Paper>
+        </TabPanel>
+        <TabPanel value={value} index={1}>
+          <Paper>
+            <TableContainer>
+              <Table stickyHeader>
+                <TableHead>
+                  <TableRow>
+                    <TableCell align="center">Quiz Name</TableCell>
+                    <TableCell align="center">Start Time</TableCell>
+                    <TableCell align="center">Quiz Home Page</TableCell>
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  {upcomingQuizzes.map(quiz => (
+                    <TableRow>
+                      <TableCell align="center">{quiz.name}</TableCell>
+                      <TableCell align="center">
+                        {new Date(quiz.startTime).toLocaleDateString('en-US', {
+                          minute: '2-digit',
+                          hour: 'numeric',
+                          day: 'numeric',
+                          month: 'long',
+                          year: 'numeric',
+                        })}
+                      </TableCell>
+                      <TableCell align="center">
+                        <Button
+                          onClick={() => history.push(`/quiz/${quiz.id}/home`)}
+                          variant="contained"
+                          color="primary"
+                        >
+                          visit
+                        </Button>
+                      </TableCell>
+                    </TableRow>
+                  ))}
+                </TableBody>
+              </Table>
+            </TableContainer>
+          </Paper>
+        </TabPanel>
       </Container>
     </>
   );
